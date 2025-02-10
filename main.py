78fc90a6a469fc790ef83e3774c7a97dc0986e4b
Adrian Liechti <adrian@monobox.ch>
2025-02-10 22:34:05 +0100

    add openai config

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ import tempfile
 import uvicorn
 
 from fastapi import FastAPI, Form, HTTPException, Response, UploadFile
+from openai import OpenAI
 from markitdown import MarkItDown
 
 app = FastAPI(
     title="LLM Platform Extractor"
 )
+
+llm_client = None
+llm_model = os.getenv("OPENAI_MODEL")
+
+openai_url = os.getenv("OPENAI_BASE_URL")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+if openai_url or openai_api_key:
+    llm_client = OpenAI(api_key=openai_api_key, base_url=openai_url)
+
+markitdown = MarkItDown(llm_model=llm_model, llm_client=llm_client)
 
 @app.post("/")
 async def create_upload_file(file: UploadFile = None, files: UploadFile = None, format: str = Form("markdown")):
@@ -24,14 +36,18 @@ async def create_upload_file(file: UploadFile = None, files: UploadFile = None, 
         
         match format:
             case "text" | "markdown":
-                markitdown = MarkItDown()
-                result = markitdown.convert(file_path)
-                
-                return [
-                    {
-                        "text": result.text_content
-                    }
-                ]
+                try:
+                    result = markitdown.convert(file_path)
+
+                    return [
+                        {
+                            "title": result.title,
+                            "text": result.text_content
+                        }
+                    ]
+                except Exception as e:
+                    app.logger.error(f"Error processing file {file_name}: {e}")
+                    raise HTTPException(status_code=500, detail="Internal server error")
             
             case _:
                 raise HTTPException(status_code=400, detail="Invalid format")
