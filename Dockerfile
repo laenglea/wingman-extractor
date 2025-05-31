f7e710e92777d9756890b5c7d7672924c94d5cfd
Adrian Liechti <adrian@monobox.ch>
2025-06-01 01:03:06 +0200

    switch to grpc

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,9 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cpu
 
-COPY main.py .
+COPY *.py .
 
-EXPOSE 8000
+EXPOSE 50051
+VOLUME /app/.cache
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os
 import tempfile
-import uvicorn
+import grpc
+import mimetypes
 
-from fastapi import FastAPI, Form, HTTPException, Response, UploadFile
+from concurrent import futures
+from grpc_reflection.v1alpha import reflection
+
 from openai import OpenAI
 from markitdown import MarkItDown
 
-app = FastAPI(
-    title="LLM Platform Extractor"
-)
+import extractor_pb2
+import extractor_pb2_grpc
 
 llm_client = None
 llm_model = os.getenv("OPENAI_MODEL")
@@ -21,36 +23,56 @@ if openai_url or openai_api_key:
 
 markitdown = MarkItDown(llm_model=llm_model, llm_client=llm_client)
 
-@app.post("/")
-async def create_upload_file(file: UploadFile = None, files: UploadFile = None, format: str = Form("markdown")):
-    file = file or files
-    
-    file_name = file.filename
-    file_content = await file.read()
+class ExtractorServicer(extractor_pb2_grpc.ExtractorServicer):
+    def Extract(self, request: extractor_pb2.ExtractRequest, context: grpc.aio.ServicerContext):
+        file = request.file
+        format = request.format or extractor_pb2.FORMAT_TEXT
 
-    with tempfile.TemporaryDirectory() as temp_dir:
-        file_path = os.path.join(temp_dir, file_name)
-    
-        with open(file_path, "wb") as temp_file:
-            temp_file.write(file_content)
+        if not file:
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            context.set_details('File is required but not provided')
+            return extractor_pb2.File()
         
-        match format:
-            case "text" | "markdown":
-                try:
-                    result = markitdown.convert(file_path)
+        if format is not extractor_pb2.FORMAT_TEXT:
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            context.set_details('Format must be FORMAT_TEXT')
+            return extractor_pb2.File()
+        
+        file_ext = mimetypes.guess_extension(file.content_type) or '.tmp'
+        file_name = "input" + file_ext
 
-                    return [
-                        {
-                            "title": result.title,
-                            "text": result.text_content
-                        }
-                    ]
-                except Exception as e:
-                    app.logger.error(f"Error processing file {file_name}: {e}")
-                    raise HTTPException(status_code=500, detail="Internal server error")
+        with tempfile.TemporaryDirectory() as temp_dir:
+            file_path = os.path.join(temp_dir, file_name)
+
+            with open(file_path, "wb") as temp_file:
+                temp_file.write(file.content)
             
-            case _:
-                raise HTTPException(status_code=400, detail="Invalid format")
+            result = markitdown.convert(file_path)
+            data = bytes(result.text_content, 'utf-8')
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+            with open('page.md', 'wb') as f:
+                f.write(data)
+
+            return extractor_pb2.File(content=data, content_type='text/markdown')
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    extractor = ExtractorServicer()
+    extractor_pb2_grpc.add_ExtractorServicer_to_server(extractor, server)
+
+    SERVICE_NAMES = (
+        extractor_pb2.DESCRIPTOR.services_by_name['Extractor'].full_name,
+        reflection.SERVICE_NAME,
+    )
+
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
+
+    server.add_insecure_port('[::]:50051')
+    server.start()
+
+    print("Wingman Extractor started. Listening on port 50051.")
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
